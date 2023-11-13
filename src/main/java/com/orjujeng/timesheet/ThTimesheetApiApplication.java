eda17a83811bc908ccd20e93bc176eec27390d1d
orjujeng <orjujeng@hotmail.com>
2023-11-13 22:09:13 +0800

    first commit

---
+++ src/main/java/com/orjujeng/timesheet/ThTimesheetApiApplication.java
@@ -0,0 +1,18 @@
+package com.orjujeng.timesheet;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.orjujeng.timesheet.mapper")
+public class ThTimesheetApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ThTimesheetApiApplication.class, args);
+	}
+}
+
+
